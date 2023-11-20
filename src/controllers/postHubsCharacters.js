012143e63bab1e6e23ffb8b0c2e3ebd41e1dfb01
Gabriel <git@github.com>
2023-11-20 17:41:01 -0500

    added hubspot module

---
+++ src/controllers/postHubsCharacters.js
@@ -0,0 +1,9 @@
+import { Client } from "@hubspot/api-client";
+const hubspotClient = new Client({ accessToken: YOUR_ACCESS_TOKEN });
+
+const postHubsCharacters = async () => {
+    
+
+};
+
+module.exports = postHubsCharacters;
