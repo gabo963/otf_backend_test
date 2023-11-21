a29a8e60080048f018c02510d5b8e00fd42ed71e
Gabo <gsarmiento101874@gmail.com>
2023-11-21 17:09:45 -0500

    changed contacts to batch, and added associations

--- src/controllers/postHubspotContacts.js
+++ src/controllers/postHubspotContacts.js
@@ -1,34 +1,55 @@
-const hubspot = require('@hubspot/api-client')
-require("dotenv").config()
-const {SOURCE_ACCESS_TOKEN} = process.env;
+const hubspot = require("@hubspot/api-client");
+require("dotenv").config();
+const { SOURCE_ACCESS_TOKEN } = process.env;
 
-const postHubspotContacts = async ( contacts ) => {
-    
-    const hubspotClient = new hubspot.Client({ accessToken: SOURCE_ACCESS_TOKEN });
+const postHubspotContacts = async (contacts, associations) => {
+  const hubspotClient = new hubspot.Client({
+    accessToken: SOURCE_ACCESS_TOKEN,
+  });
 
-    const promises = []
-    const locations = {};
+  const promises = [];
+  const locations = {};
 
-    for( let i = 0; i < contacts.length; i++) {
-        const {character_id, firstname, lastname, status_character, character_species, character_gender, location} = contacts[i];
-        locations[character_id] = location;
-        const promise = hubspotClient.crm.contacts.basicApi.create({
-            "properties": {
-                character_id, firstname, lastname, status_character, character_species, character_gender
-        }});
-        promises.push(promise);
-    }
+  const contactsToBeSentOut = contacts.map((contact) => {
+    const {
+      character_id,
+      firstname,
+      lastname,
+      status_character,
+      character_species,
+      character_gender,
+      location,
+    } = contact;
+    return {
+      properties: {
+        character_id,
+        firstname,
+        lastname,
+        status_character,
+        character_species,
+        character_gender,
+      },
+      associations: [
+        {
+          types: [
+            {
+              associationCategory: "HUBSPOT_DEFINED",
+              associationTypeId: 1,
+            },
+          ],
+          to: { id: associations[location] },
+        },
+      ],
+    };
+  });
 
-    result = await Promise.all(promises);
+  const call = await hubspotClient.crm.contacts.batchApi.create({
+    inputs: contactsToBeSentOut,
+  });
 
-    const created = {};
+  console.log(call.results);
 
-    result.forEach( response => {
-        const id = parseInt(response.properties.character_id);
-        created[id] = [response.id, locations[id]];
-    });
-
-    return created;
+  return call.results;
 };
 
 module.exports = postHubspotContacts;
