2e8700c54d584e8bc8922f5a5f8a18d12e817f94
Gabo <gsarmiento101874@gmail.com>
2023-11-21 18:31:06 -0500

    pending batch companies

--- src/controllers/postHubspotContacts.js
+++ src/controllers/postHubspotContacts.js
@@ -3,53 +3,31 @@ require("dotenv").config();
 const { SOURCE_ACCESS_TOKEN } = process.env;
 
 const postHubspotContacts = async (contacts, associations) => {
-  const hubspotClient = new hubspot.Client({
-    accessToken: SOURCE_ACCESS_TOKEN,
-  });
+    const hubspotClient = new hubspot.Client({ accessToken: SOURCE_ACCESS_TOKEN });
 
-  const promises = [];
-  const locations = {};
+    const contactsToBeSentOut = contacts.map((contact) => {
+        const { character_id, firstname, lastname, status_character, character_species, character_gender, location } = contact;
+        return {
+            properties: { character_id, firstname, lastname, status_character, character_species, character_gender },
+            associations: [
+                {
+                    types: [
+                        {
+                            associationCategory: "HUBSPOT_DEFINED",
+                            associationTypeId: 1,
+                        },
+                    ],
+                    to: { id: associations[location] },
+                },
+            ],
+        };
+    });
 
-  const contactsToBeSentOut = contacts.map((contact) => {
-    const {
-      character_id,
-      firstname,
-      lastname,
-      status_character,
-      character_species,
-      character_gender,
-      location,
-    } = contact;
-    return {
-      properties: {
-        character_id,
-        firstname,
-        lastname,
-        status_character,
-        character_species,
-        character_gender,
-      },
-      associations: [
-        {
-          types: [
-            {
-              associationCategory: "HUBSPOT_DEFINED",
-              associationTypeId: 1,
-            },
-          ],
-          to: { id: associations[location] },
-        },
-      ],
-    };
-  });
+    const call = await hubspotClient.crm.contacts.batchApi.create({
+        inputs: contactsToBeSentOut,
+    });
 
-  const call = await hubspotClient.crm.contacts.batchApi.create({
-    inputs: contactsToBeSentOut,
-  });
-
-  console.log(call.results);
-
-  return call.results;
+    return call.results;
 };
 
 module.exports = postHubspotContacts;
--- src/handlers/migrationHandler.js
+++ src/handlers/migrationHandler.js
@@ -4,73 +4,70 @@ const postHubspotCompanies = require("../controllers/postHubspotCompanies");
 const postHubspotContacts = require("../controllers/postHubspotContacts");
 
 const checkMigration = async (req, res) => {
-  try {
-    const contacts = [
-      {
-        character_id: 1,
-        firstname: "Rick",
-        lastname: "Sanchez",
-        status_character: "Alive",
-        character_species: "Human",
-        character_gender: "Male",
-        location: 1,
-      },
-      {
-        character_id: 2,
-        firstname: "Morty",
-        lastname: "Smith",
-        status_character: "Alive",
-        character_species: "Human",
-        character_gender: "Male",
-        location: 3,
-      },
-    ];
+    try {
+        const contacts = [
+            {
+                character_id: 1,
+                firstname: "Rick",
+                lastname: "Sanchez",
+                status_character: "Alive",
+                character_species: "Human",
+                character_gender: "Male",
+                location: 1,
+            },
+            {
+                character_id: 2,
+                firstname: "Morty",
+                lastname: "Smith",
+                status_character: "Alive",
+                character_species: "Human",
+                character_gender: "Male",
+                location: 3,
+            },
+        ];
 
-    const companies = [
-      {
-        location_id: 1,
-        name: "Earth (C-137)",
-        location_type: "Planet",
-        dimension: "Dimension C-137",
-        creation_date: "2017-11-10",
-      },
-      {
-        location_id: 3,
-        name: "Abadango",
-        location_type: "Cluster",
-        dimension: "unknown",
-        creation_date: "2012-11-10",
-      },
-    ];
+        const companies = [
+            {
+                location_id: 1,
+                name: "Earth (C-137)",
+                location_type: "Planet",
+                dimension: "Dimension C-137",
+                creation_date: "2017-11-10",
+            },
+            {
+                location_id: 3,
+                name: "Abadango",
+                location_type: "Cluster",
+                dimension: "unknown",
+                creation_date: "2012-11-10",
+            },
+        ];
 
-    const companyResponse = await postHubspotCompanies(companies);
-    const contactResponse = await postHubspotContacts(
-      contacts,
-      companyResponse
-    );
-    res.status(200).json({
-      response: "Hola Mundo, el backend esta funcionando.",
-      companies: companyResponse,
-      contacts: contactResponse,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(404).json({ error: error.message });
-  }
+        const companyResponse = await postHubspotCompanies(companies);
+        const contactResponse = await postHubspotContacts(contacts, companyResponse);
+        res.status(200).json({
+            response: "Hola Mundo, el backend esta funcionando.",
+            companies: companyResponse,
+            contacts: contactResponse,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({ error: error.message });
+    }
 };
 
 const migrate = async (req, res) => {
-  try {
-    const characters = await getCharactersToMigrate();
-    const locations = await getLocationsToMigrate();
+    try {
+        const characters = await getCharactersToMigrate();
+        const locations = await getLocationsToMigrate();
 
-    res.status(200).json({ characters, locations });
+        res.status(200).json({ characters, locations });
 
-    // aca corre la migracion
-  } catch (error) {
-    console.log(error);
-    res.status(404).json({ error: error.message });
-  }
+        // aca corre la migracion
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({ error: error.message });
+    }
 };
 
 module.exports = { checkMigration, migrate };
