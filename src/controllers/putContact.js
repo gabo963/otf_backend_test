8a8963cac3184d9bdb4fe9b13a8f5661f51117f7
Gabo <gsarmiento101874@gmail.com>
2023-11-21 23:36:43 -0500

    added put contact

---
+++ src/controllers/putContact.js
@@ -0,0 +1,7 @@
+const hubspot = require("@hubspot/api-client");
+require("dotenv").config();
+const { MIRROR_ACCESS_TOKEN } = process.env;
+
+const putContact = async (newContactInfo) => {};
+
+module.exports = putContact;
