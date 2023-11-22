9bc553ace755a8f0c8b667362207bbb10a32ff12
Gabo <gsarmiento101874@gmail.com>
2023-11-21 21:06:12 -0500

    adjusted the migration handler

--- src/handlers/migrationHandler.js
+++ src/handlers/migrationHandler.js
@@ -5,51 +5,7 @@ const postHubspotContacts = require("../controllers/postHubspotContacts");
 
 const checkMigration = async (req, res) => {
     try {
-        const contacts = [
-            {
-                character_id: 1,
-                firstname: "Rick",
-                lastname: "Sanchez",
-                status_character: "Alive",
-                character_species: "Human",
-                character_gender: "Male",
-                location: 1,
-            },
-            {
-                character_id: 2,
-                firstname: "Morty",
-                lastname: "Smith",
-                status_character: "Alive",
-                character_species: "Human",
-                character_gender: "Male",
-                location: 3,
-            },
-        ];
-
-        const companies = [
-            {
-                location_id: 1,
-                name: "Earth (C-137)",
-                location_type: "Planet",
-                dimension: "Dimension C-137",
-                creation_date: "2017-11-10",
-            },
-            {
-                location_id: 3,
-                name: "Abadango",
-                location_type: "Cluster",
-                dimension: "unknown",
-                creation_date: "2012-11-10",
-            },
-        ];
-
-        const companyResponse = await postHubspotCompanies(companies);
-        const contactResponse = await postHubspotContacts(contacts, companyResponse);
-        res.status(200).json({
-            response: "Hola Mundo, el backend esta funcionando.",
-            companies: companyResponse,
-            contacts: contactResponse,
-        });
+        res.status(200).json({ response: "Hola Mundo, el backend esta funcionando." });
     } catch (error) {
         console.log(error);
         res.status(404).json({ error: error.message });
@@ -60,8 +16,10 @@ const migrate = async (req, res) => {
     try {
         const characters = await getCharactersToMigrate();
         const locations = await getLocationsToMigrate();
+        const companyResponse = await postHubspotCompanies(locations);
+        const contactResponse = await postHubspotContacts(characters, companyResponse);
 
-        res.status(200).json({ characters, locations });
+        res.status(200).json({ companies: companyResponse, contacts: contactResponse });
 
         // aca corre la migracion
     } catch (error) {
