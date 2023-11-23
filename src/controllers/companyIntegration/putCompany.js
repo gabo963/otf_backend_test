8a722826c2b864f25bcf907a78ad02ab66b719d1
Gabo <gsarmiento101874@gmail.com>
2023-11-22 21:29:55 -0500

    adjusted bug

--- src/controllers/companyIntegration/putCompany.js
+++ src/controllers/companyIntegration/putCompany.js
@@ -5,7 +5,7 @@ const { MIRROR_ACCESS_TOKEN } = process.env;
 const putCompany = async (newCompanyInfo) => {
     const hubspotClient = new hubspot.Client({ accessToken: MIRROR_ACCESS_TOKEN });
 
-    const { name, dimension, location_id, creation_date, location_type } = newContactInfo;
+    const { name, dimension, location_id, creation_date, location_type } = newCompanyInfo;
 
     const response = await hubspotClient.crm.companies.basicApi.update(newCompanyInfo.id, {
         properties: { name, dimension, location_id, creation_date, location_type },
